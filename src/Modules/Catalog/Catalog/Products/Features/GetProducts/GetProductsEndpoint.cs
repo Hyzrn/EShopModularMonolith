d7fb134299312678d1175549f263a2e07905c09a
Hyzrn <selahattin_hayzaran@hotmail.com>
2024-11-02 12:17:31 +0300

    Created get products endpoint

---
+++ src/Modules/Catalog/Catalog/Products/Features/GetProducts/GetProductsEndpoint.cs
@@ -0,0 +1,22 @@
+namespace Catalog.Products.Features.GetProducts;
+
+public sealed record GetProductResponse(IEnumerable<ProductDto> Products);
+
+public class GetProductsEndpoint : ICarterModule
+{
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        app.MapGet("/products", async (ISender sender) =>
+        {
+            var result = await sender.Send(new GetProductsQuery());
+            var response = result.Adapt<GetProductResponse>();
+
+            return Results.Ok(response);
+        })
+        .WithName("GetProducts")
+        .Produces<GetProductResponse>(StatusCodes.Status200OK)
+        .ProducesProblem(StatusCodes.Status400BadRequest)
+        .WithSummary("Get Products")
+        .WithDescription("Get Products");;
+    }
+}
